6569198d8b1c33159770ca28b33d390b27cefa4f
Kishore Pashindla <kishore.pashindla@gmail.com>
2022-06-22 10:53:06 +0100

    Updated mapping method

--- src/main/java/com/ab/controllers/HomeController.java
+++ src/main/java/com/ab/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.ab.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
