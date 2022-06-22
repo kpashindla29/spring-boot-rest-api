7990c409d75ba51801ead845b2bfb74c9ace9793
Kishore Pashindla <kishore.pashindla@gmail.com>
2022-06-22 10:51:40 +0100

    Updated mapping method

--- src/main/java/com/ab/controllers/HomeController.java
+++ src/main/java/com/ab/controllers/HomeController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 	
-	@GetMapping("/")
-	public String greeting() {
+	@GetMapping("/{name}")
+	public String greeting(@PathVariable("name") String name) {
 		
-		return "Hello";
+		return "Hello "+name;
 	}
 
 }
